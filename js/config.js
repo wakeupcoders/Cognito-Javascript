a0affae83da2bd80f819969a46ec6b90dbe9a4ff
narayan jha <aadityamonu67@gmail.com>
2020-05-26 20:06:13 +0530

    first commit

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_g7wXYANLP', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '5lmqrrti4rh6iptf62snrd310h' //is this used anywhere?
+    },
+};
+
